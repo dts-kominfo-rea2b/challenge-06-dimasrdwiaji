87a1ab412f02e3f3a0e0e7255f278f5f3f0af8fe
dimasrdwiaji <79129785+dimasrdwiaji@users.noreply.github.com>
2022-06-10 16:12:19 +0700

    ya ampun kok susah ya

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require('fs');
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,47 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+let splittedArr = [];
+
+function bacaData(fnCallback) {
+  let fileList = [file1, file2, file3];
+  for (const element of fileList) {
+    fs.readFile(element, 'utf-8', (err, data) => {
+      if (err) {
+        return console.log(`Error : ` + err);
+      } else {
+        const parsedData = JSON.parse(data);
+        if (parsedData.message !== undefined) {
+          let splittedData = parsedData.message.split(' ');
+          const lastWord = splittedData[1];
+          fnCallback(err, lastWord);
+      
+        // Condition for JSON file like data2.json
+        } else if (parsedData[0].message !== undefined) {
+          let splittedData = parsedData[0].message.split(' ');
+          const lastWord = splittedData[1];
+          fnCallback(err, lastWord);
+      
+        // Condition for JSON file like data3.json
+        } else if (parsedData[0]['data'].message !== undefined) {
+          let splittedData = parsedData[0]['data'].message.split(' ');
+          const lastWord = splittedData[1];
+          fnCallback(err, lastWord);
+        }
+
+        function fnCallback(err, secondWord) {
+          if (err) {
+            return console.log(`Error : ` + err);
+          } else {
+            splittedArr.push(secondWord);
+            console.log(splittedArr);
+          }
+        }
+      }
+    })
+  }
+}
+
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
